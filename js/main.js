53f84e5f689bbf626e3f0beea35bad6e77619fb9
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-21 17:20:17 +0100

    add image con v-for

--- js/main.js
+++ js/main.js
@@ -25,3 +25,17 @@ const images = [
         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     },
 ];
+
+
+// Ciao ragazzi,
+// Esercizio di oggi: Vue Slider
+// nome repo: vue-slider
+// Descrizione:
+// Partendo dal markup HTML allegato creare uno slider di immagini in VueJS.
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+// Consigli del giorno:
+// - regola d'oro: riciclare ovunque possibile!
+// - il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
